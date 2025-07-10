5ab8e943abc9e7235221c167120bebdf1bcebc70
Dylan Bridgman <light@ether.org.za>
2025-07-10 16:00:46 +0200

    Save humanity

--- src/main/java/com/kapil/jpl/exceptions/BreakException.java
+++
@@ -1,6 +0,0 @@
-package com.kapil.jpl.exceptions;
-
-public class BreakException extends RuntimeException {
-    public BreakException() { super(); }
-
-}
--- src/main/java/com/kapil/jpl/exceptions/JPLException.java
+++
@@ -1,7 +0,0 @@
-package com.kapil.jpl.exceptions;
-
-public class JPLException extends RuntimeException {
-    public JPLException(String message) {
-        super("🛑 JPL Error: " + message);
-    }
-}
